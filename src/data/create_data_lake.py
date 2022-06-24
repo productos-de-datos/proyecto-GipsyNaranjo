8bd2a7e32655746054715ec384cdb5de0eb880b2
GipsyNaranjo <gtnaranjod@unal.edu.co>
2022-06-23 20:03:23 -0500

    solution

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -1,4 +1,9 @@
+"""creacion de la carpeta `data_lake junto con sus respectivos directorios, para crear la carpeta de la libreria os la funcion os.mkdir, las subcarpetas fueron definidas en listas, para luego ser creadas la funcion path.join en 
+una list comprehension"""
+
+
 def create_data_lake():
+
     """Cree el data lake con sus capas.
 
     Esta función debe crear la carpeta `data_lake` en la raiz del proyecto. El data lake contiene
@@ -18,20 +23,16 @@ def create_data_lake():
               |___ forecasts/
 
     ```
-    
-
-
     """
     import os
+
     os.mkdir("./data_lake")
-    carpetas = ["landing","raw", "cleansed", "business"]
-    carpetas_business = ["reports", "features", "forecast"]
-    carpetas_reports = ["figures"]
-    [os.mkdir(os.path.join("data_lake/",c)) for c in carpetas]
-    [os.mkdir(os.path.join("data_lake/business/",c)) for c in carpetas_business]
+    carpetas = ["landing", "raw", "cleansed","business"]
+    carpetas_business = ["reports", "features", "forecasts"]
+    [os.mkdir(os.path.join("data_lake/", c)) for c in carpetas]
+    [os.mkdir(os.path.join("data_lake/business/", c)) for c in carpetas_business]
     os.mkdir("./data_lake/business/reports/figures")
     return
-
     raise NotImplementedError("Implementar esta función")
 
 if __name__ == "__main__":
